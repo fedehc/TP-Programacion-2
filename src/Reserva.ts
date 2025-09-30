96b7eb6aa0bdf1faaec1641898bdc7b9de594dd0
Melina De Marte <demartemeli@gmail.com>
2025-09-29 21:40:22 -0300

    Agrego clase Reserva

---
+++ src/Reserva.ts
@@ -0,0 +1,8 @@
+export default class Reserva{
+  private id: string;
+  private cliente: Cliente;
+  private vehiculo: Vehiculo;
+  private fechaInicio: date:
+  private fechaFin: date:
+  private estado: EstadoReserva = EstadoReserva.PENDIENTE;
+}
