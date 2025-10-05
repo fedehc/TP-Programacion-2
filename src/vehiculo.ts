4e872c39c170fd15c4b5f9b454120b868da1b9a7
FedeHC <fede.hc@mail.com>
2025-10-05 16:27:52 -0300

    Agregando y renombrado campos en clase Vehiculo

--- src/vehiculo.ts
+++ src/vehiculo.ts
@@ -1,15 +1,19 @@
 import Tarifa from "./tarifa";
 import Mantenimiento from "./mantenimiento";
+import { CategoriaVehiculo } from "./enums";
 
 
 export default abstract class Vehiculo {
+    private categoria: CategoriaVehiculo;
     private matricula: string;
     private estado: string;
     private kilometraje: number;
     private tarifa: Tarifa;
-    private historial: Mantenimiento[];
+    private mantenimientos: Mantenimiento[];
 
 
+    getCategoria(): CategoriaVehiculo {  return this.categoria; }
+    setCategoria(nuevaCat: CategoriaVehiculo) { this.categoria = nuevaCat; }
     getMatricula(): string {  return this.matricula; }
     setMatricula(nuevaMat: string) { this.matricula = nuevaMat; }
     getEstado(): string { return this.estado; }
@@ -18,14 +22,17 @@ export default abstract class Vehiculo {
     setKilometraje(nuevoKm: number) { this.kilometraje = nuevoKm; }
     getTarifa(): Tarifa { return this.tarifa; }
     setTarifa(nuevaTarifa: Tarifa) { this.tarifa = nuevaTarifa; }  
-    getHistorial(): Mantenimiento[] { return this.historial; }
-    setHistorial(nuevoHistorial: Mantenimiento[]) { this.historial = nuevoHistorial; }
+    getMantenimiento(): Mantenimiento[] { return this.mantenimientos; }
+    setMantenimiento(nuevoMant: Mantenimiento[]) { this.mantenimientos = nuevoMant; }
 
-    constructor(matricula: string, estado: string, kilometraje: number, tarifa: Tarifa, historial: Mantenimiento[]) {
+
+    constructor(categoria: CategoriaVehiculo, matricula: string, estado: string, kilometraje: number,
+                tarifa: Tarifa, mantenimientos: Mantenimiento[]) {
+        this.categoria = categoria;
         this.matricula = matricula;
         this.estado = estado;
         this.kilometraje = kilometraje;
         this.tarifa = tarifa;
-        this.historial = historial;
+        this.mantenimientos = mantenimientos;
     }
 }
