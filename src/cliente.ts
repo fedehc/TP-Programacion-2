aafd8d91e8416dffa356f89026d790580549d745
FedeHC <fede.hc@mail.com>
2025-10-12 19:04:54 -0300

    Refactorizando constructor y reduciendo lineas sin cambiar lógica en clase Cliente

--- src/cliente.ts
+++ src/cliente.ts
@@ -1,38 +1,23 @@
 export default class Cliente {
+    constructor(
+        private id: number,
+        private apellido: string,
+        private nombre: string,
+        private dni: number,
+        private telefono: string,
+        private email: string
+    ) { }
 
-    private id: number;
-    private apellido: string;
-    private nombre: string;
-    private dni: number;
-    private telefono: string;
-    private email: string;
-
-    
     public setId(id: number): void { this.id = id; }
     public getId(): number { return this.id; }
-
     public setApellido(apellido: string): void { this.apellido = apellido; }
     public getApellido(): string { return this.apellido; }
-
     public setNombre(nombre: string): void { this.nombre = nombre; }
     public getNombre(): string { return this.nombre; }
-
     public setDni(dni: number): void { this.dni = dni; }
     public getDni(): number { return this.dni; }
-
     public setTelefono(telefono: string): void { this.telefono = telefono; }
     public getTelefono(): string { return this.telefono; }
-
     public setEmail(email: string): void { this.email = email; }
     public getEmail(): string { return this.email; }
-
-
-    constructor(id: number, apellido: string, nombre: string, dni: number, telefono: string, email: string) {
-        this.id = id;
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.dni = dni;
-        this.telefono = telefono;
-        this.email = email;
-    }
 }
