f403c407077d7cf99d99ebd129e6e7f44654d778
FedeHC <fede.hc@mail.com>
2025-10-12 19:39:28 -0300

    Refactorizando constructor y reduciendo lineas sin cambiar lógica en clase TarifaSUV

--- src/tarifaSuv.ts
+++ src/tarifaSuv.ts
@@ -1,19 +1,20 @@
 import Tarifa from "./tarifa";
 
-export default  class TarifaSUV implements Tarifa {
-  private baseDia = 80;
-  private seguroDia = 15;
-  private excedenteKm = 0.25;
+
+export default class TarifaSUV implements Tarifa {
+  constructor(
+    private baseDia = 80,
+    private seguroDia = 15,
+    private excedenteKm = 0.25
+  ) { }
+
 
   calcularCosto(dias: number, kmRecorridos: number): number {
-
     const base = this.baseDia * dias;
     const seguro = this.seguroDia * dias;
-
     const limite = 500;
     const exceso = Math.max(0, kmRecorridos - limite);
     const variable = exceso * this.excedenteKm;
-
     return base + seguro + variable;
   }
 }
