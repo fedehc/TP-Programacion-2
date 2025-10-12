94def3b3ad377b029b8d5dfb604d7d1897a3ad16
Melina De Marte <demartemeli@gmail.com>
2025-10-12 11:05:52 -0300

    Normalizo nombres a minúscula

--- src/sistemaAlquiler.ts
+++ src/sistemaAlquiler.ts
@@ -1,23 +1,11 @@
-import RangoDeFechas from "./rangoDeFechas";
-import Reserva from "./Reserva";
-import Vehiculo from "./vehiculo";
+import { Reserva } from "./reserva";
 
 export default class SistemaAlquiler {
+  
+  private readonly clientes: Cliente[] = [];
+  private readonly vehiculos: Vehiculo[] = [];
+  private readonly reservas: Reserva[] = [];
+  private readonly alquileres: Alquiler[] = [];
+  private readonly mantenimientos: Mantenimiento[] = [];
 
-  private vehiculos: Vehiculo[] = [];
-  private reservas: Reserva[] = [];
-
-  public crearReservaPendiente(clienteId: string, fechaInicio: Date, fechaFin: Date): Reserva {
-    const rango = new RangoDeFechas(fechaInicio, fechaFin);
-    const nueva = new Reserva(this.generarIdReserva(), clienteId, rango);
-    // Todavía no se guarda, hasta saber si se puede confirmar.
-    return nueva;
   }
-  private generarIdReserva(): string {
-    return "R-" + Date.now();
-  }
-
-
-
-}
-
