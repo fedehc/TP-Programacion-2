51a4eb335ca8cc98e3c0e0c7e3faa9aaa20f60c1
FedeHC <fede.hc@mail.com>
2025-10-12 19:38:49 -0300

    Refactorizando constructor y reduciendo lineas sin cambiar lógica en clase TarifaSedan

--- src/tarifaSedan.ts
+++ src/tarifaSedan.ts
@@ -1,14 +1,16 @@
 import Tarifa from "./tarifa";
 
+
 export default class TarifaSedan implements Tarifa {
-  private baseDia = 50;
-  private porKm = 0.20;
+  constructor(
+    private baseDia = 50,
+    private porKm = 0.20
+  ) { }
+
 
   calcularCosto(dias: number, kmRecorridos: number): number {
-
     const base = this.baseDia * dias;
     const variable = kmRecorridos * this.porKm;
-
     return base + variable;
   }
 }
