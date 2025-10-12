66d8d55530f268373d4ed0e233fa2f80c3bb28e6
FedeHC <fede.hc@mail.com>
2025-10-12 19:32:37 -0300

    Refactorizando constructor y reduciendo lineas sin cambiar lógica en clase Reserva

--- src/reserva.ts
+++ src/reserva.ts
@@ -4,54 +4,21 @@ import Vehiculo from "./vehiculo";
 
 
 export default class Reserva {
-
-  private id: string;
-  private clienteId: string;
-  private rango: RangoDeFechas;
-  private estado: EstadoReserva;
-  private vehiculo?: Vehiculo;
-
   constructor(
-    id: string,
-    clienteId: string,
-    rango: RangoDeFechas,
-    estado: EstadoReserva = EstadoReserva.pendiente,
-    vehiculo?: Vehiculo
-  ) {
-    this.id = id;
-    this.clienteId = clienteId;
-    this.rango = rango;
-    this.estado = estado;
-    this.vehiculo = vehiculo;
-  }
+    private id: string,
+    private clienteId: string,
+    private rango: RangoDeFechas,
+    private estado: EstadoReserva = EstadoReserva.pendiente,
+    private vehiculo?: Vehiculo
+  ) { }
 
-  public getId(): string {
-    return this.id;
-  }
-
-  public getClienteId(): string {
-    return this.clienteId;
-  }
-
-  public getRango(): RangoDeFechas {
-    return this.rango;
-  }
-
-  public getEstado(): EstadoReserva {
-    return this.estado;
-  }
-
-  public getVehiculo(): Vehiculo | undefined {
-    return this.vehiculo;
-  }
-
-  public getVehiculoMatricula(): string | undefined {
-    return this.vehiculo?.getMatricula();
-  }
-
-  public marcarCumplida() {
-    this.estado = EstadoReserva.confirmada;
-  }
+  public getId(): string { return this.id; }
+  public getClienteId(): string { return this.clienteId; }
+  public getRango(): RangoDeFechas { return this.rango; }
+  public getEstado(): EstadoReserva { return this.estado; }
+  public getVehiculo(): Vehiculo | undefined { return this.vehiculo; }
+  public getVehiculoMatricula(): string | undefined { return this.vehiculo?.getMatricula(); }
+  public marcarCumplida() { this.estado = EstadoReserva.confirmada; }
 
   public confirmarConVehiculo(v: Vehiculo): void {
     this.vehiculo = v;
