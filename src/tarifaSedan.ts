0c9e361972df20a2bae8bbdb0f4a6e09abbdd0bb
Melina De Marte <demartemeli@gmail.com>
2025-10-12 13:20:37 -0300

    se modifica lofica de costo

--- src/tarifaSedan.ts
+++ src/tarifaSedan.ts
@@ -1,45 +1,14 @@
 import Tarifa from "./tarifa";
 
+export default class TarifaSedan implements Tarifa {
+  private baseDia = 50;
+  private porKm = 0.20;
 
-export default class TarifaSedan extends Tarifa {
+  calcularCosto(dias: number, kmRecorridos: number): number {
 
-    private listaKmsDiarios: number[] = [];
-    private costoBaseDiario: number = 50;
-    private recargoKmsDiarios: number = 0.2;
+    const base = this.baseDia * dias;
+    const variable = kmRecorridos * this.porKm;
 
-
-    public getListaKmsDiarios(): number[] { return this.listaKmsDiarios; }
-    public setListaKmsDiarios(listaKmsDiarios: number[]): void { this.listaKmsDiarios = listaKmsDiarios; }
-    public getCostoBaseDiario(): number { return this.costoBaseDiario; }
-    public getRecargoKmsDiarios(): number { return this.recargoKmsDiarios; }
-
-
-    public calcularRecargoTotal(): number {
-        let totalRecargo: number = 0;
-
-        for (const kmsDia of this.getListaKmsDiarios()) {
-            totalRecargo += kmsDia * this.getRecargoKmsDiarios();
-        }
-
-        return totalRecargo;
-    }
-
-    public calcularCostoKmsTotal(): number {
-        let cantDias : number = this.getListaKmsDiarios().length;
-        let total: number = cantDias * this.getCostoBaseDiario();        
-
-        return total;
-    }
-
-    public calcularTarifaTotal(): number {
-        return this.calcularRecargoTotal() + this.calcularCostoKmsTotal();
-    }
-
-    
-    constructor()
-    constructor(listaKmsDiarios: number[])
-    constructor(listaKmsDiarios?: number[]) {
-        super();
-        if(listaKmsDiarios) { this.listaKmsDiarios = listaKmsDiarios; }
-    }
+    return base + variable;
+  }
 }
