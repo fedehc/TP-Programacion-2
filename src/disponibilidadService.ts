cde9e035b8e18baa5db193faf782e33c72be4507
FedeHC <fede.hc@mail.com>
2025-10-12 19:07:06 -0300

    Agregando llaves omitidas en clase DisponibilidadService

--- src/disponibilidadService.ts
+++ src/disponibilidadService.ts
@@ -1,9 +1,12 @@
 import RangoDeFechas from "./rangoDeFechas";
 
+
 export default class DisponibilidadService {
   static estaLibre(rangoPedido: RangoDeFechas, bloqueos: RangoDeFechas[]): boolean {
     for (const b of bloqueos) {
-      if (rangoPedido.seCruzaCon(b)) return false;
+      if (rangoPedido.seCruzaCon(b)) {
+        return false;
+      }
     }
     return true;
   }
