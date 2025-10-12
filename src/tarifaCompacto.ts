581bb0dff88843e6db7a8f70b096f6508fa2a14e
Melina De Marte <demartemeli@gmail.com>
2025-10-12 13:19:50 -0300

    se modifica lofica de costo

--- src/tarifaCompacto.ts
+++ src/tarifaCompacto.ts
@@ -1,48 +1,16 @@
 import Tarifa from "./tarifa";
 
+export default class TarifaCompacto implements Tarifa {
+  private baseDia = 30;
+  private excedenteKm = 0.15;
 
-export default class TarifaCompacto extends Tarifa {
+  calcularCosto(dias: number, kmRecorridos: number): number {
 
-    private listaKmsDiarios: number[] = [];
-    private costoBaseDiario: number = 30;
-    private limiteKmsDiarios: number = 100;
-    private recargoKmsDiarios: number = 0.15;
+    const base = this.baseDia * dias;
+    const limite = 100 * dias;
+    const exceso = Math.max(0, kmRecorridos - limite);
+    const variable = exceso * this.excedenteKm;
 
-
-    public getListaKmsDiarios(): number[] { return this.listaKmsDiarios; }
-    public setListaKmsDiarios(listaKmsDiarios: number[]): void { this.listaKmsDiarios = listaKmsDiarios; }
-    public getCostoBaseDiario(): number { return this.costoBaseDiario; }
-    public getLimiteKmsDiarios(): number { return this.limiteKmsDiarios; }
-    public getRecargoKmsDiarios(): number { return this.recargoKmsDiarios; }
-
-
-    public calcularRecargoTotal(): number {
-        let totalRecargo: number = 0;
-
-        for (const kmsDia of this.getListaKmsDiarios()) {
-            if (kmsDia && kmsDia > this.getLimiteKmsDiarios()) {
-                totalRecargo += (kmsDia - this.getLimiteKmsDiarios()) * this.getRecargoKmsDiarios();
-            }
-        }
-        return totalRecargo;
-    }
-
-    public calcularCostoKmsTotal(): number {
-        let cantDias: number = this.getListaKmsDiarios().length;
-        let total: number = cantDias * this.getCostoBaseDiario();        
-
-        return total;
-    }
-
-    public calcularTarifaTotal(): number {
-        return this.calcularRecargoTotal() + this.calcularCostoKmsTotal();
-    }
-    
-    
-    constructor()
-    constructor(listaKmsDiarios: number[])
-    constructor(listaKmsDiarios?: number[]) {
-        super();
-        if(listaKmsDiarios) { this.listaKmsDiarios = listaKmsDiarios; }
-    }
+    return base + variable;
+  }
 }
