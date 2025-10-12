9a7543fe1d6f02b6596e0c3328e7818b5129f7d0
FedeHC <fede.hc@mail.com>
2025-10-12 19:40:42 -0300

    Reduciendo lineas en clase Vehiculo

--- src/vehiculo.ts
+++ src/vehiculo.ts
@@ -2,25 +2,20 @@ import { CategoriaVehiculo, EstadoVehiculo } from "./enums";
 import Tarifa from "./tarifa";
 
 
-
 export default class Vehiculo {
   constructor(
     private matricula: string,
     private categoria: CategoriaVehiculo,
     private estado: EstadoVehiculo = EstadoVehiculo.disponible,
     private tarifa: Tarifa,
-    private kilometraje: number,
-  ) {}
+    private kilometraje: number
+  ) { }
 
   public getMatricula(): string { return this.matricula; }
-  public getCategoria(): CategoriaVehiculo{ return this.categoria; }
+  public getCategoria(): CategoriaVehiculo { return this.categoria; }
   public getEstado(): EstadoVehiculo { return this.estado; }
   public getTarifa(): Tarifa { return this.tarifa; }
   public getKilometraje(): number { return this.kilometraje; }
-
   public setEstado(nuevo: EstadoVehiculo) { this.estado = nuevo; }
-
-  public setKilometraje(nuevoKilometraje: number){
-    this.kilometraje = nuevoKilometraje;
-  }
+  public setKilometraje(nuevoKilometraje: number) { this.kilometraje = nuevoKilometraje; }
 }
