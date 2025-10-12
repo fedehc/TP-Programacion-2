5013daab364d72fb57db289648799c01904e17cc
Melina De Marte <demartemeli@gmail.com>
2025-10-12 13:21:26 -0300

    se modifica lofica de costo

--- src/tarifaSuv.ts
+++ src/tarifaSuv.ts
@@ -1,51 +1,19 @@
 import Tarifa from "./tarifa";
 
+export default  class TarifaSUV implements Tarifa {
+  private baseDia = 80;
+  private seguroDia = 15;
+  private excedenteKm = 0.25;
 
-export default class TarifaSuv extends Tarifa {
+  calcularCosto(dias: number, kmRecorridos: number): number {
 
-    private listaKmsDiarios: number[] = [];
-    private costoBaseDiario: number = 80;
-    private costoSeguroPorDia = 15;
-    private limiteKms: number = 500;
-    private recargoKms: number = 0.25;
+    const base = this.baseDia * dias;
+    const seguro = this.seguroDia * dias;
 
+    const limite = 500;
+    const exceso = Math.max(0, kmRecorridos - limite);
+    const variable = exceso * this.excedenteKm;
 
-    public getListaKmsDiarios(): number[] { return this.listaKmsDiarios; }
-    public setListaKmsDiarios(listaKmsDiarios: number[]): void { this.listaKmsDiarios = listaKmsDiarios; }
-    public getCostoBaseDiario(): number { return this.costoBaseDiario; }
-    public getCostoSeguroPorDia(): number { return this.costoSeguroPorDia; }
-    public getLimiteKms(): number { return this.limiteKms; }
-    public getRecargoKms(): number { return this.recargoKms; }
-
-    public calcularRecargoTotal(): number {
-        let totalRecargo: number = 0;
-        let totalKmsRecorridos: number = 0;
-
-        for (const kmsDia of this.getListaKmsDiarios()) {
-            totalKmsRecorridos += kmsDia;
-        }
-        if(totalKmsRecorridos > this.getLimiteKms()) {
-            totalRecargo = (totalKmsRecorridos - this.getLimiteKms()) * this.getRecargoKms();
-        }
-        return totalRecargo;
-    }
-
-    public calcularCostoKmsTotal(): number {
-        let cantDias: number = this.getListaKmsDiarios().length;
-        let total: number = cantDias * (this.getCostoBaseDiario() + this.getCostoSeguroPorDia());        
-
-        return total;
-    }
-
-    public calcularTarifaTotal(): number {
-        return this.calcularRecargoTotal() + this.calcularCostoKmsTotal();
-    }
-
-    
-    constructor()
-    constructor(listaKmsDiarios: number[])
-    constructor(listaKmsDiarios?: number[]) {
-        super();
-        if(listaKmsDiarios) { this.listaKmsDiarios = listaKmsDiarios; }
-    }
+    return base + seguro + variable;
+  }
 }
