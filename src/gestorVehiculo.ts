be66584d490e4ee880c433b3cdb9f18cc1101f78
FedeHC <fede.hc@mail.com>
2025-10-12 19:16:54 -0300

    Refactorizando constructor y reduciendo lineas sin cambiar lógica en clase GestorVehiculo

--- src/gestorVehiculo.ts
+++ src/gestorVehiculo.ts
@@ -1,34 +1,26 @@
 import { CategoriaVehiculo, EstadoVehiculo } from "./enums";
 import Vehiculo from "./vehiculo";
 
+
 export default class GestorVehiculo {
-  private vehiculos: Vehiculo[] = [];
+  constructor(
+    private vehiculos: Vehiculo[] = []
+  ) { }
 
-  public agregar(v: Vehiculo): void {
-    this.vehiculos.push(v);
-  }
 
-  public listar(): Vehiculo[] {
-    return this.vehiculos;
-  }
+  public agregar(v: Vehiculo): void { this.vehiculos.push(v); }
+  public listar(): Vehiculo[] { return this.vehiculos; }
+  public marcarEnAlquiler(v: Vehiculo): void { v.setEstado(EstadoVehiculo.alquiler); }
+  public marcarLimpieza(v: Vehiculo): void { v.setEstado(EstadoVehiculo.limpieza); }
+  public actualizarKm(v: Vehiculo, km: number): void { v.setKilometraje(km); }
 
   public filtrarPorCategoria(categoria: CategoriaVehiculo): Vehiculo[] {
     const listaFiltrada: Vehiculo[] = [];
     for (const v of this.vehiculos) {
-      if (v.getCategoria() === categoria) listaFiltrada.push(v);
+      if (v.getCategoria() === categoria) {
+        listaFiltrada.push(v);
+      }
     }
     return listaFiltrada;
-  }
-
-  public marcarEnAlquiler(v: Vehiculo): void {
-    v.setEstado(EstadoVehiculo.alquiler);
-  }
-
-  public marcarLimpieza(v: Vehiculo): void {
-    v.setEstado(EstadoVehiculo.limpieza);
-  }
-
-  public actualizarKm(v: Vehiculo, km: number): void {
-    v.setKilometraje(km);
   }
 }
